20488b6cec2dfb4d1dc9d26bfb02e28992e492c7
Alex Parlato <alexparlato@stamen.com>
2022-05-19 16:34:36 -0400

    Set up repo (#8)

---
+++ src/components/ControlPanel.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="panel" />
+
+<style>
+  .panel {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 360px;
+    left: 0;
+    border-right: 1px solid #666;
+  }
+</style>
