2942e6a22d39bfad6ef48e4d98f7bb87a3f2fa66
Alex Parlato <alparlat@3c063012601d.ant.amazon.com>
2022-09-30 17:00:28 -0400

    simple all maneuver behavior

--- src/navigate-route.js
+++ src/navigate-route.js
@@ -285,7 +285,8 @@ const navigate = (map, options) => {
         if (!easeInPitch) easeInPitch = map.getPitch();
         if (!easeInZoom) easeInZoom = map.getZoom();
 
-        const maneuverBehavior = maneuverOptions?.[nextManeuver?.type];
+        const maneuverBehavior =
+          maneuverOptions?.['*'] || maneuverOptions?.[nextManeuver?.type];
 
         const easedPosition = easePitchAndZoom(
           {
